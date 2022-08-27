059fad365fda6c68d25933dd2981bd2f4433dceb
davalbra <davalbra@espol.edu.ec>
2022-08-27 12:18:42 -0500

    Agregando la class img

---
+++ src/app/component/img/img.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.css'],
+})
+export class ImgComponent implements OnInit {
+  @Input() img: string = 'valor init';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
